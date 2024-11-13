496f6e42f718df9929bbdc3fc8793a33bd30b655
WaffleBananas <pvzmasterdavid@gmail.com>
2024-11-13 00:58:27 -0800

    Update InternalVertex.java

--- graph/InternalVertex.java
+++ graph/InternalVertex.java
@@ -2,4 +2,13 @@ package graph;
 
 public class InternalVertex {
     
+	public Object realVertex;               //Holds the real vertex object
+	public DList edgeList = new DList();    //Holds all of the edges 
+	
+	//Internal vertex needs to hold both the real Vertex object, and also a DList containing the edges
+	
+	public InternalVertex(Object vertex) {
+		realVertex = vertex;
+	}
+
 }
