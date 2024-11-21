68243004747ebccd18562dee765ec14a8563787d
e-ko123 <ethan.h.ko@gmail.com>
2024-11-20 19:49:50 -0800

    add halfedge pointer working on removedge for now

--- graph/HalfEdge.java
+++ graph/HalfEdge.java
@@ -1,10 +1,13 @@
 package graph;
 
+import list.DListNode;
+
 public class HalfEdge {
     public HalfEdge siblingEdge;
     public int weight;
     public InternalVertex internalVert1;
     public InternalVertex internalVert2; 
+    public DListNode parentDListNode;
 
     public HalfEdge(InternalVertex in_internalVert1, InternalVertex in_internalVert2, int in_weight){
         weight = in_weight;
@@ -15,5 +18,10 @@ public class HalfEdge {
     public void setSiblingEdge(HalfEdge sibling) {
         siblingEdge = sibling;
         sibling.siblingEdge = siblingEdge;
+    }
+
+    public DListNode getParentDListNode()
+    {
+        return parentDListNode;
     }
 }
