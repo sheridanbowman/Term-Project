f584e2c9b5cc3083342801900f1e33f7230f32a0
WaffleBananas <pvzmasterdavid@gmail.com>
2024-11-21 21:01:51 -0800

    Update Edge.java

--- graphalg/Edge.java
+++ graphalg/Edge.java
@@ -1,7 +1,7 @@
 package graphalg;
 
 // non-split instance of edge, pointing to it's real objects
-public class Edge {
+public class Edge implements Comparable<Edge> {
     public int weight;
     public Object internalVert1;
     public Object internalVert2; 
@@ -10,6 +10,17 @@ public class Edge {
         weight = w;
         internalVert1 = u;
         internalVert2 = v;
+    }
+    
+    //Overrides the default compareTo method in order for the mergesort algorithm to work
+    public int compareTo(Edge edge1, Edge edge2) {
+        if(edge1.weight < edge2.weight) {
+        	return -1;
+        } else if(edge1.weight == edge2.weight) {
+        	return 0;
+        } else if(edge1.weight > edge2.weight) {
+        	return 1;
+        }
     }
 
     //todo needs Comparable implementation on weight for linkedQueue's sorts to be able to sort it
